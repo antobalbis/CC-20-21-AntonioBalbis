727ef538308b8228dab843d9eca954e109a1cf68
Antonio Balbís <antoniobalh@gmail.com>
2020-11-01 19:37:02 +0100

    Añadidos setters a localidad #8

--- src/Localidad.scala
+++ src/Localidad.scala
@@ -6,11 +6,27 @@ class Localidad (name : String, id : Int){
   var precipitaciones: Double = 0
   var humedad: Double = 0
 
-  def getNombre(): String = ""
-  def getID(): Int = 0
-  def getTemperatura(): Double = ""
-  def getViento(): Double = 0.0
-  def getPrecipitaciones(): Double = 0.0
-  def getHumedad(): Double = 0.0
+  def getNombre(): String = nombre
+  def getID(): Int = estacion_id
+  def getTemperatura(): Double = temperatura
+  def getViento(): Double = viento
+  def getPrecipitaciones(): Double = precipitaciones
+  def getHumedad(): Double = humedad
+  
+  def setTemperatura(temperatura: Double){
+  	this.temperatura = temperatura
+  }
+  
+  def setViento(viento: Double){
+  	this.viento = viento
+  }
+  
+  def setPrecipitaciones(precipitaciones: Double){
+  	this.precipitaciones = precipitaciones
+  }
+  
+  def setHumedad(humedad: Double){
+  	this.humedad = humedad
+  }
 }
 
