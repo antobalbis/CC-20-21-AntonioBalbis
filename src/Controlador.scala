75bcd1ff6245d5adcd51c99eea3149434631bbbc
Antonio Balbís <antoniobalh@gmail.com>
2020-11-01 19:16:33 +0100

    Añadidos métodos de envío de datos #8

--- src/Controlador.scala
+++ src/Controlador.scala
@@ -1,7 +1,8 @@
 class Controlador(){
-	var ultimaFecha : String = ""
-	var ultimaActualizacion : String = ""
+	var ultimaFecha: String = ""
+	var ultimaActualizacion: String = ""
 	
-	def getPlaceInfo(nombre : String) : String = ""
-	
+	def getPlaceInfo(nombre: String) : String = ""
+	def sendMeteoInfo(nombre: String, temperatura:Double, viento: Double, 
+				precipitaciones: Double, humedad: Double): Boolean = false
 }
--- src/Localidad.scala
+++ src/Localidad.scala
@@ -1,12 +1,14 @@
 class Localidad (name : String, id : Int){
   val nombre : String = name
   val estacion_id : Int = id
+  var temperatura: Double = 0
   var viento: Double = 0
   var precipitaciones: Double = 0
   var humedad: Double = 0
 
   def getNombre(): String = ""
   def getID(): Int = 0
+  def getTemperatura(): Double = ""
   def getViento(): Double = 0.0
   def getPrecipitaciones(): Double = 0.0
   def getHumedad(): Double = 0.0
