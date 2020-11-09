b9ed7679584bfddcff15c9c39dd68d504e1c6e98
Antonio Balbís <antoniobalh@gmail.com>
2020-11-09 22:18:31 +0100

    Corregido error en la declaración

--- src/Solicitud.scala
+++ src/Solicitud.scala
@@ -1,9 +1,9 @@
-class Solicitud(id: String, nombre: String, desc: String, int npersonas){
+class Solicitud(id: String, nombre: String, desc: String, npersonas: Int){
 	val ID: String = id
 	val nombre_solicitante: String = nombre
 	val nPersonas: Int = nPersonas
 	var descripción: String = desc
-	var restantes:Int = nPersonas
+	var restantes: Int = nPersonas
 	
 	def getID():String = ID
 	def getNombreSolicitante():String = nombre_solicitante
