4ff27a0f4b17eac04b22ffbb6f89cb9dbed5a436
Antonio Balbís <antoniobalh@gmail.com>
2020-11-09 22:10:48 +0100

    Añadida clase maquina #6 #7

---
+++ src/Maquina.scala
@@ -0,0 +1,20 @@
+import EstadoMaquina._
+
+class Maquina(ID: String, nombre:String){
+	val ID: String = ID
+	val nombre: String = nombre
+	var isBeingUsed: Boolean = false
+	var estado = FUNCIONANDO
+	
+	def getNombre():String = nombre
+	def getID():String = ID
+	def isInUse():Boolean = isBeingUsed
+	
+	def startUsing() {
+		if(!isBeingUsed) isBeingUsed = true
+	}
+	
+	def stopUsing(){
+		isBeingUsed = false
+	}
+}
