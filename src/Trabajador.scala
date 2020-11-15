68373edde1e8fa55f30b0ad27e8b5d00f0fa4e2f
Antonio Balbís <antoniobalh@gmail.com>
2020-11-15 18:14:42 +0100

    Añadido método para crear solicitud #25

--- src/Trabajador.scala
+++ src/Trabajador.scala
@@ -3,5 +3,7 @@ import Departamento._
 class Trabajador (name: String, depart: Departamento){
 	val nombre: String = name
 	val departamento = depart
+
+	def crearSolicitud(String descripcion){}
 	
 }
