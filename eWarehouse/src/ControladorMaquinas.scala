9cd463b1bd3d68c30deadf169ed764c1cf1f798c
Antonio Balbís <antoniobalh@gmail.com>
2021-08-05 18:24:28 +0200

    Creado método para añadir máquina #27 #39

--- eWarehouse/src/ControladorMaquinas.scala
+++ eWarehouse/src/ControladorMaquinas.scala
@@ -3,6 +3,17 @@ package eWarehouse
 class ControladorMaquinas(){
 	val listaMaquinas : List[Maquina] = List()
 
-	def addMaquina(maquina : Maquina){}
+	def addMaquina(maquina : Maquina){
+		if(!maquinaExist(maquina.ID)) maquina +: listaMaquinas
+	}
+
 	def removeMaquina (id : String){}
+
+	def maquinaExist(id : Int) : Boolean = {
+		var exist : Boolean = false
+		for(maquina <- listaMaquinas){
+			if(maquina.ID == id) exist = true
+		}
+		exist
+	}
 }
