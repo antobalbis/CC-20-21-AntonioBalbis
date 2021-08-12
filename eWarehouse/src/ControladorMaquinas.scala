8be17781b23bf718328681f711da791a7d77fdf4
Antonio Balbís <antoniobalh@gmail.com>
2021-08-12 17:15:24 +0200

    Método para solicitar la lista de máquinas #6 #38

---
+++ eWarehouse/src/ControladorMaquinas.scala
@@ -0,0 +1,27 @@
+package eWarehouse
+
+class ControladorMaquinas(){
+	val listaMaquinas : List[Maquina] = List()
+
+	def addMaquina(maquina : Maquina, tipo){
+		if(!maquinaExist(maquina.ID)) maquina +: listaMaquinas
+	}
+
+	def removeMaquina (id : String){}
+
+	def maquinaExist(id : Int) : Boolean = {
+		var exist : Boolean = false
+		for(maquina <- listaMaquinas){
+			if(maquina.ID == id) exist = true
+		}
+		exist
+	}
+
+	def getListaMaquinas() : List[Maquina] = {
+		val result : List[Maquina] = List()
+		for(maquina <- listaMaquinas){
+			if(maquina.estado == "FUN") result +: maquina
+		}
+		result
+	}
+}
