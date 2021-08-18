158e316638916610b6c05f21d3b568fa357e09de
Antonio Balbís <antoniobalh@gmail.com>
2021-08-18 13:22:52 +0200

    eliminados métodos setters y de cambio de valor

--- eWarehouse/src/Maquina.scala
+++ eWarehouse/src/Maquina.scala
@@ -7,19 +7,4 @@ class Maquina(id : Integer, name : String){
 	val nombre: String = name
 	var isBeingUsed: Boolean = false
 	var estado : EstadoMaquina = FUNCIONANDO
-
-	def obtenerEstado() : EstadoMaquina = estado
-
-	def cambiarEstado(status: EstadoMaquina){
-		estado = status
-	}
-
-	def getIsBeingUsed() : Boolean = getIsBeingUsed
-	def startUsing(){
-		isBeingUsed = false
-	}
-
-	def stopUsing(){
-		isBeingUsed = true
-	}
 }
