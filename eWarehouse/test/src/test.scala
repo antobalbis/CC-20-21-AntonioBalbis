2c7508344842cdf539a675fb9ca1cf2384dd34eb
Antonio Balbís <antoniobalh@gmail.com>
2021-08-18 12:11:06 +0200

    Añadidos test para comprobar la consulta de la lista de máquinas #6 #38

--- eWarehouse/test/src/test.scala
+++ eWarehouse/test/src/test.scala
@@ -17,4 +17,18 @@ class CMtests extends AnyFunSuite{
     cm.addMaquina(3, "maquina_fallo")
     assert(cm.listaMaquinas.length == 5)
   }
+
+  test("No se muestra ninguna máquina con estado distinto a FUNCIONANDO"){
+    cm.listaMaquinas(3).estado = EstadoMaquina.PENDIENTE
+    assert(!cm.getListaMaquinas().contains(cm.listaMaquinas(3)))
+  }
+
+  test("Comprobar que se muestran todas las máquinas con estado FUNCIONANDO"){
+    /*var result : Boolean = true
+    val lista : List[Maquina] = cm.getListaMaquinas()
+    for(maquina <- cm.listaMaquinas){
+      if(maquina.estado.equals(EstadoMaquina.FUNCIONANDO)) result = lista.contains(maquina)
+    }*/
+    assert(cm.getListaMaquinas().exists(m => m.estado.equals(EstadoMaquina.FUNCIONANDO)))
+  }
 }
