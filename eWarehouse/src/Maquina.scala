f4f8781d48392e27632673581400aefcc12e58c5
Antonio Balbís <antoniobalh@gmail.com>
2021-08-18 13:28:10 +0200

    Añadida variable de id de usuario

--- eWarehouse/src/Maquina.scala
+++ eWarehouse/src/Maquina.scala
@@ -5,6 +5,7 @@ import EstadoMaquina._
 class Maquina(id : Integer, name : String){
 	val ID: Integer = id
 	val nombre: String = name
+	var userID : Integer = -1	
 	var isBeingUsed: Boolean = false
 	var estado : EstadoMaquina = FUNCIONANDO
 }
