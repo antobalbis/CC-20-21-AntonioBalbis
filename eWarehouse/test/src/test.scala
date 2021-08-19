e43666aa59d994d61e17ad8336801b20124298dd
Antonio Balbís <antoniobalh@gmail.com>
2021-08-19 12:32:49 +0200

    Añadidos test de #43

--- eWarehouse/test/src/test.scala
+++ eWarehouse/test/src/test.scala
@@ -24,14 +24,10 @@ class CMtests extends AnyFunSuite{
   }
 
   test("Comprobar que se muestran todas las máquinas con estado FUNCIONANDO"){
-    /*var result : Boolean = true
-    val lista : List[Maquina] = cm.getListaMaquinas()
-    for(maquina <- cm.listaMaquinas){
-      if(maquina.estado.equals(EstadoMaquina.FUNCIONANDO)) result = lista.contains(maquina)
-    }*/
     assert(cm.getListaMaquinas().exists(m => m.estado.equals(EstadoMaquina.FUNCIONANDO)))
   }
 
+  //TESTS USO MÄQUINA
   test("El estado de la máquina con el id especificado cambia a true"){
     assert(cm.usarMaquina(2, 0))
     assert(cm.listaMaquinas(cm.listaMaquinas.indexWhere(m => m.ID == 2)).isBeingUsed)
@@ -51,5 +47,24 @@ class CMtests extends AnyFunSuite{
 
   test("Comprobar si el resultado es true que el id del usuario es igual al id del usuario de la máquina."){
     assert(cm.listaMaquinas(cm.listaMaquinas.indexWhere(m => m.ID == 2)).userID == 0)
+  }
+
+  //TESTS DEJAR MÁQUINA
+  test("Comprobar que si el resultado es true el valor de isBeingUsed pasa a false"){
+    assert(cm.dejarMaquina(2, 0))
+    assert(cm.listaMaquinas(cm.listaMaquinas.indexWhere(m => m.ID == 2)).isBeingUsed)
+  }
+
+  test("Comprobar que si la máquina no existe el resultado es false"){
+    assert(!cm.dejarMaquina(-1, 0))
+  }
+
+  test("Comprobar que si el resultado es true el valor de user id es -1"){
+    assert(cm.listaMaquinas(cm.listaMaquinas.indexWhere(m => m.ID == 2)).userID == -1)
+  }
+
+  test("Comprobar que si no coincide el userID con el userID de la máquina el resultado es false"){
+    cm.usarMaquina(2, 0)
+    assert(!cm.dejarMaquina(2, 1))
   }
 }
