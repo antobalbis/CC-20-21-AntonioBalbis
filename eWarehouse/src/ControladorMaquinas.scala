cd9010ec87260e3550852c3292e4808ba38c8f86
Antonio Balbís <antoniobalh@gmail.com>
2021-08-19 13:30:50 +0200

    Añadido id de usuario como parámetro de los métodos y test que comprueba que si el usuario no existe no se añade la máquina #39

--- eWarehouse/src/ControladorMaquinas.scala
+++ eWarehouse/src/ControladorMaquinas.scala
@@ -4,12 +4,12 @@ class ControladorMaquinas(){
 	var listaMaquinas : List[Maquina] = List()
 	var listaTrabajadores : List[Trabajador] = List()
 
-	def addMaquina(id : Int, nombre : String){
+	def addMaquina(userID: Int, id : Int, nombre : String){
 		val maquina : Maquina = new Maquina(id, nombre)
 		if(getMaquina(maquina.ID) == -1) listaMaquinas = maquina :: listaMaquinas
 	}
 
-	def deleteMaquina(id : Int){
+	def deleteMaquina(userID : Int, id : Int){
 		val index : Int = getMaquina(id)
 		listaMaquinas = listaMaquinas.take(index) ++ listaMaquinas.drop(index + 1)
 	}
@@ -22,7 +22,7 @@ class ControladorMaquinas(){
 		index
 	}
 
-	def getListaMaquinas() : List[Maquina] = {
+	def getListaMaquinas(userID: Int) : List[Maquina] = {
 		var result : List[Maquina] = List()
 		for(maquina <- listaMaquinas){
 			if(maquina.estado.equals(EstadoMaquina.FUNCIONANDO)) result = maquina :: result
--- eWarehouse/test/src/test.scala
+++ eWarehouse/test/src/test.scala
@@ -4,59 +4,71 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class CMtests extends AnyFunSuite{
   var cm : ControladorMaquinas = new ControladorMaquinas()
+  var lt : List[Trabajador] = List()
+  lt = new Trabajador(0, "aa", Departamento.DIRECCION) :: lt
+  lt = new Trabajador(1, "ab", Departamento.LOGISTICA) :: lt
+  lt = new Trabajador(2, "ac", Departamento.MANTENIMIENTO) :: lt
+
+  cm.listaTrabajadores = lt
 
   for(i <- 0 to 4){
-    cm.addMaquina(i, "maquina-"+i)
+    cm.addMaquina(0, i, "maquina-"+i)
   }
 
+  //TEST ADD MAQUINAS
   test("Lista de máquinas debe tener 5 elementos") {
     assert(cm.listaMaquinas.length == 5)
   }
 
   test("No se añade una máquina con id repetido"){
-    cm.addMaquina(3, "maquina_fallo")
+    cm.addMaquina(1, 3, "maquina_fallo")
     assert(cm.listaMaquinas.length == 5)
+  }
+
+  test("Comprobar que no se añade una máquina si el usuario no existe o no es del departamento de logística."){
+    cm.addMaquina(userID = 7, id = 5, "maquina 5")
+    assert(!cm.listaMaquinas.exists(m => m.ID == 5))
   }
 
   test("No se muestra ninguna máquina con estado distinto a FUNCIONANDO"){
     cm.listaMaquinas(3).estado = EstadoMaquina.PENDIENTE
-    assert(!cm.getListaMaquinas().contains(cm.listaMaquinas(3)))
+    assert(!cm.getListaMaquinas(0).contains(cm.listaMaquinas(3)))
   }
 
   test("Comprobar que se muestran todas las máquinas con estado FUNCIONANDO"){
-    assert(cm.getListaMaquinas().exists(m => m.estado.equals(EstadoMaquina.FUNCIONANDO)))
+    assert(cm.getListaMaquinas(0).exists(m => m.estado.equals(EstadoMaquina.FUNCIONANDO)))
   }
 
   //TESTS USO MÄQUINA
   test("El estado de la máquina con el id especificado cambia a true"){
-    assert(cm.usarMaquina(2, 0))
+    assert(cm.usarMaquina(2, 3))
     assert(cm.listaMaquinas(cm.listaMaquinas.indexWhere(m => m.ID == 2)).isBeingUsed)
   }
 
   test("Comprobar que si el valor de isBeingUsed es true el resultado es false"){
-    assert(!cm.usarMaquina(2, 0))
+    assert(!cm.usarMaquina(2, 3))
   }
 
   test("Comprobar que si la máquina no existe el resultado es false"){
-    assert(!cm.usarMaquina(-1, 0))
+    assert(!cm.usarMaquina(-1, 3))
   }
 
   test("Comprobar que si el estado de la máquina es distinto a FUNCIONANDO el resultado es false"){
-    assert(!cm.usarMaquina(cm.listaMaquinas(3).ID, 0))
+    assert(!cm.usarMaquina(cm.listaMaquinas(3).ID, 3))
   }
 
   test("Comprobar si el resultado es true que el id del usuario es igual al id del usuario de la máquina."){
-    assert(cm.listaMaquinas(cm.listaMaquinas.indexWhere(m => m.ID == 2)).userID == 0)
+    assert(cm.listaMaquinas(cm.listaMaquinas.indexWhere(m => m.ID == 2)).userID == 3)
   }
 
   //TESTS DEJAR MÁQUINA
   test("Comprobar que si el resultado es true el valor de isBeingUsed pasa a false"){
-    assert(cm.dejarMaquina(2, 0))
+    assert(cm.dejarMaquina(2, 3))
     assert(!cm.listaMaquinas(cm.listaMaquinas.indexWhere(m => m.ID == 2)).isBeingUsed)
   }
 
   test("Comprobar que si la máquina no existe el resultado de dejarMaquina es false"){
-    assert(!cm.dejarMaquina(-1, 0))
+    assert(!cm.dejarMaquina(-1, 3))
   }
 
   test("Comprobar que si el resultado es true el valor de user id es -1"){
