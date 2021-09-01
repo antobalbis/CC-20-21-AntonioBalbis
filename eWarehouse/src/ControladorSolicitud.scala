68888ba9a3df09d35f4ce240dc592099d7b651fc
Antonio Balbís <antoniobalh@gmail.com>
2021-09-01 23:46:22 +0200

    añadido método para añadir solicitud #25 #49

--- eWarehouse/src/ControladorSolicitud.scala
+++ eWarehouse/src/ControladorSolicitud.scala
@@ -1,8 +1,27 @@
 package eWarehouse
 
 class ControladorSolicitud{
-	val listaSolicitudes: List[Solicitud] = List()
+	var listaSolicitudes: List[Solicitud] = List()
+	var listaTrabajadores : List[Trabajador] = List()
 
-	def addSolicitud(solicitud: Solicitud) = {}
+	def existTrabajador(userID : Int) : Boolean = {
+		listaTrabajadores.exists(t => t.ID ==userID)
+	}
+
+	def checkDepartment(userID : Int, esperado : Departamento.Value) : Boolean = {
+		listaTrabajadores.exists(t => t.ID == userID && t.departamento.equals(esperado))
+	}
+
+	def existSolicitud(id : Int) : Boolean = {
+		listaSolicitudes.exists(s => s.ID == id)
+	}
+
+	def addSolicitud(userID : Int, id: Int, desc : String, nTrabajadores : Int) = {
+		if (checkDepartment(userID, Departamento.LOGISTICA) && !existSolicitud(id)) {
+			val nombre : String = listaTrabajadores(listaTrabajadores.indexWhere(t => t.ID == userID)).nombre
+			listaSolicitudes = new Solicitud(id, nombre, desc,nTrabajadores) :: listaSolicitudes
+		}
+	}
+
 	def removeSolicitud(id: String) = {}
 }
