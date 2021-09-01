34e9053b0cacb46c1628a9f238129675803fa0cd
Antonio Balbís <antoniobalh@gmail.com>
2021-09-02 00:59:09 +0200

    cambio de nombre por id #25

--- eWarehouse/src/Solicitud.scala
+++ eWarehouse/src/Solicitud.scala
@@ -1,10 +1,9 @@
 package eWarehouse
 
-class Solicitud(id: Int, nombre: String, desc: String, nTrabajadores: Int){
+class Solicitud(id: Int, userid: Int, desc: String, nTrabajadores: Int){
 	val ID: Int = id
-	val nombre_solicitante: String = nombre
-	val trabajadores : List[Trabajador] = List()
-	val nPersonas: Int = 0
+	val userID: Int = userid
+	var trabajadores : List[Int] = List()
 	var descripción: String = desc
-	var restantes: Int = nPersonas
+	var restantes: Int = nTrabajadores
 }
