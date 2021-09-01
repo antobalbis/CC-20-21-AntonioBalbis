9d2f87d69e8118117311a3a93a299f7c96fa5c04
Antonio Balbís <antoniobalh@gmail.com>
2021-09-02 01:00:17 +0200

    corregido error en los tests de #50

--- eWarehouse/test/src/testControladorSolicitud.scala
+++ eWarehouse/test/src/testControladorSolicitud.scala
@@ -20,7 +20,7 @@ class CSTest extends AnyFunSuite{
   }
 
   test("Comprobar que si el trabajador no existe no se añade la solicitud"){
-    cs.addSolicitud(5, 10, "solicitud falsa", 3)
+    cs.addSolicitud(-1, 10, "solicitud falsa", 3)
     assert(!cs.listaSolicitudes.exists(s => s.ID == 10))
   }
 
@@ -38,13 +38,14 @@ class CSTest extends AnyFunSuite{
   lt = new Trabajador(3, "ac", Departamento.LOGISTICA) :: lt
   lt = new Trabajador(4, "ac", Departamento.LOGISTICA) :: lt
   lt = new Trabajador(5, "ac", Departamento.LOGISTICA) :: lt
-
+  lt = new Trabajador(6, "ac", Departamento.LOGISTICA) :: lt
+  cs.listaTrabajadores = lt
 
   test("Comprobar que el trabajador se añade a la lista de trabajadores de la solicitud y el número de personas disminuye en 1."){
-    val nPersonas = cs.getSolicitudByID(0).nPersonas
-    cs.apuntarseSolicitud(1, 0)
-    assert(cs.getSolicitudByID(0).trabajadores.exists(t => t.ID == 1))
-    assert(nPersonas == cs.getSolicitudByID(0).nPersonas - 1)
+    val nPersonas = cs.getSolicitudByID(0).restantes
+    cs.apuntarseSolicitud(3, 0)
+    assert(cs.getSolicitudByID(0).trabajadores.exists(t => t == 3))
+    assert(nPersonas - 1 == cs.getSolicitudByID(0).restantes)
   }
 
   test("Comprobar que si el trabajador no existe no se añade a la lista y el número de personas se mantiene."){
@@ -52,18 +53,32 @@ class CSTest extends AnyFunSuite{
   }
 
   test("Comprobar que si el trabajador ya está en la lista el número de personas restantes se mantiene."){
-    val nPersonas = cs.getSolicitudByID(0).nPersonas
-    cs.apuntarseSolicitud(1, 0)
-    assert(nPersonas == cs.getSolicitudByID(0).nPersonas)
+    val nPersonas = cs.getSolicitudByID(0).restantes
+    cs.apuntarseSolicitud(3, 0)
+    assert(nPersonas == cs.getSolicitudByID(0).restantes)
   }
 
   test("Comprobar que si se ha alcanzado el número de trabajadores no se añada a otro trabajador."){
-    cs.apuntarseSolicitud(3, 0)
     cs.apuntarseSolicitud(4, 0)
-    //max apuntados
-    val nPersonas = cs.getSolicitudByID(0).nPersonas
     cs.apuntarseSolicitud(5, 0)
-    assert(nPersonas == cs.getSolicitudByID(0).nPersonas)
-    assert(!cs.getSolicitudByID(0).trabajadores.exists(t => t.ID == 5))
+    //max apuntados
+    val nPersonas = cs.getSolicitudByID(0).restantes
+    cs.apuntarseSolicitud(6, 0)
+    assert(nPersonas == cs.getSolicitudByID(0).restantes)
+    assert(!cs.getSolicitudByID(0).trabajadores.exists(t => t == 6))
+  }
+
+  test("Comprobar que si el trabajador no es de logística no se añade a la lista."){
+    val nPersonas = cs.getSolicitudByID(1).restantes
+    cs.apuntarseSolicitud(0, 1)
+    assert(!cs.getSolicitudByID(1).trabajadores.exists(t => t == 0))
+    assert(nPersonas == cs.getSolicitudByID(1).restantes)
+  }
+
+  test("Comprobar que si el id es el id del solicitante no se añade a la lista."){
+    val nPersonas = cs.getSolicitudByID(1).restantes
+    cs.apuntarseSolicitud(1, 1)
+    assert(nPersonas == cs.getSolicitudByID(1).restantes)
+    assert(!cs.getSolicitudByID(1).trabajadores.exists(t => t == 1))
   }
 }
