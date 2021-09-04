85b088f391ecb3aa1d6cad6b2bdc2ead6381c138
Antonio Balbís <antoniobalh@gmail.com>
2021-09-04 13:04:47 +0200

    añadimos toString

--- eWarehouse/src/EstadoMaquina.scala
+++ eWarehouse/src/EstadoMaquina.scala
@@ -2,5 +2,8 @@ package eWarehouse
 
 object EstadoMaquina extends Enumeration{
   type EstadoMaquina=Value
-  val FUNCIONANDO,PENDIENTE,REPARACION=Value
+  val FUNCIONANDO = Value("FUNCIONANDO")
+  val PENDIENTE = Value("PENDIENTE")
+  val REPARACION=Value("REPARACION")
+
 }
