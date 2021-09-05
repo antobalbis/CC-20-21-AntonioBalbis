0c28b8bb504bfda92d86e4a40c83daf0b1e10f99
Antonio Balbís <antoniobalh@gmail.com>
2021-09-05 12:42:50 +0200

    adaptados métodos a api web

--- eWarehouse/src/ControladorSolicitud.scala
+++ eWarehouse/src/ControladorSolicitud.scala
@@ -1,11 +1,11 @@
 package eWarehouse
 
-class ControladorSolicitud{
+class ControladorSolicitud extends cask.MainRoutes{
 	var listaSolicitudes: List[Solicitud] = List()
 	var listaTrabajadores : List[Trabajador] = List()
 
 	def existTrabajador(userID : Int) : Boolean = {
-		listaTrabajadores.exists(t => t.ID ==userID)
+		listaTrabajadores.exists(t => t.ID == userID)
 	}
 
 	def checkDepartment(userID : Int, esperado : Departamento.Value) : Boolean = {
@@ -16,25 +16,48 @@ class ControladorSolicitud{
 		listaSolicitudes.exists(s => s.ID == id)
 	}
 
-	def getSolicitudByID(id : Int) : Solicitud = {
+	@cask.get("/exist/:id")
+	def existSolicitud(id : Seq[Int]) : Boolean = {
+		listaSolicitudes.exists(s => s.ID == id(0))
+	}
+
+	def getSolicitud(id : Int) : Solicitud = {
 		listaSolicitudes(listaSolicitudes.indexWhere(s => s.ID == id))
 	}
 
-	def addSolicitud(userID : Int, id: Int, desc : String, nTrabajadores : Int) = {
-		if (checkDepartment(userID, Departamento.LOGISTICA) && !existSolicitud(id)) {
-			listaSolicitudes = new Solicitud(id, userID, desc, nTrabajadores) :: listaSolicitudes
+	@cask.get("/nSolicitudes")
+	def getNSolicitudes(): Int ={
+		listaSolicitudes.size
+	}
+
+	@cask.get("/getSolicitud/:id")
+	def getSolicitudByID(id : Seq[Int]) : ujson.Obj = {
+		val solicitud = listaSolicitudes(listaSolicitudes.indexWhere(s => s.ID == id(0)))
+		ujson.Obj("userID" -> solicitud.userID.toString, "ID" -> solicitud.ID.toString,
+			"descripción" -> solicitud.descripción, "restantes" -> solicitud.restantes.toString)
+	}
+
+	@cask.postJson("/addSolicitud")
+	def addSolicitud(userID : Seq[Int], id: Seq[Int], desc : String, nTrabajadores : Seq[Int]) = {
+		if (checkDepartment(userID(0), Departamento.LOGISTICA) && !existSolicitud(id(0))) {
+			listaSolicitudes = new Solicitud(id(0), userID(0), desc, nTrabajadores(0)) :: listaSolicitudes
 		}
 	}
 
-	def apuntarseSolicitud(userID : Int, id : Int): Unit ={
-		if(checkDepartment(userID, Departamento.LOGISTICA) && existSolicitud(id)){
-			if(!(getSolicitudByID(id).userID == userID) && !getSolicitudByID(id).trabajadores.exists(t => t == userID)
-				&& getSolicitudByID(id).restantes != 0){
-				getSolicitudByID(id).trabajadores = userID :: getSolicitudByID(id).trabajadores
-				getSolicitudByID(id).restantes = getSolicitudByID(id).restantes-1
+	@cask.postJson("/apuntarse")
+	def apuntarseSolicitud(userID : Seq[Int], id : Seq[Int]): Unit ={
+		if(checkDepartment(userID(0), Departamento.LOGISTICA) && existSolicitud(id(0))){
+			var solicitud = getSolicitud(id(0))
+
+			if(!(solicitud.userID == userID(0)) && solicitud.trabajadores.exists(t => t == userID(0))
+				&& solicitud.restantes != 0){
+				solicitud.trabajadores = userID(0) :: solicitud.trabajadores
+				solicitud.restantes = solicitud.restantes-1
 			}
 		}
 	}
 
 	def removeSolicitud(id: String) = {}
+
+	initialize()
 }
